cdb006ef09d11b48d082fc0cbe7d0cb4529474d8
vincent <Vincent@mymail.sutd.edu.sg>
2018-02-16 22:34:12 +0800

    temp commit

--- datagenerator.py
+++ datagenerator.py
@@ -67,12 +67,13 @@ class ImageDataGenerator(object):
 
         # distinguish between train/infer. when calling the parsing functions
         if mode == 'training':
-            data = data.map(self._parse_function_train, num_threads=8,
-                      output_buffer_size=100*batch_size)
+            data = data.map(self._parse_function_train, num_parallel_calls=8)
+            data.prefetch(100*batch_size)
+
 
         elif mode == 'inference':
-            data = data.map(self._parse_function_inference, num_threads=8,
-                      output_buffer_size=100*batch_size)
+            data = data.map(self._parse_function_inference, num_parallel_calls=8)
+            data.prefetch(100*batch_size)
 
         else:
             raise ValueError("Invalid mode '%s'." % (mode))
